a03e410984e123088e7cfbb175660cf64b4cf2a0
Zaluznyak <72508057+Zaluznyak@users.noreply.github.com>
2021-06-25 22:53:35 +0500

    Update settings.py

--- final_project/settings.py
+++ final_project/settings.py
@@ -1,11 +1,11 @@
 import os
 import environ
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET')
 DEBUG = False
 
